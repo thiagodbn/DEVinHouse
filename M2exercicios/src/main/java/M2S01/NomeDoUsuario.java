7c6c93983c9057c768eabc85b76384a711a2213a
Noronha IntelliJ IDEA <thiagodbn@gmail.com>
2023-10-17 11:11:57 -0300

    M2S01 EX Nome do Usuario

---
+++ M2exercicios/src/main/java/M2S01/NomeDoUsuario.java
@@ -0,0 +1,17 @@
+package M2S01;
+
+import java.util.Scanner;
+
+public class NomeDoUsuario {
+    public static void main (String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite seu nome:");
+        String nome = teclado.nextLine();
+
+        System.out.print("Digite seu Sobrenome:");
+        String sobrenome = teclado.nextLine();
+
+        System.out.printf("O seu nome completo é: %s %s%n", nome, sobrenome);
+
+    }
+}
