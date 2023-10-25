55edfb31f438851acb8f6a3435c5af21c1ad69c0
Noronha IntelliJ IDEA <thiagodbn@gmail.com>
2023-10-24 21:02:52 -0300

    M2S02 EX 3 Classe Retângulo

---
+++ M2exercicios/src/main/java/M2S02/ex3/Retangulo.java
@@ -0,0 +1,61 @@
+package M2S02.ex3;
+
+public class Retangulo {
+    private double ladoA = 1;
+    private double ladoB = 1;
+    private double perimetro;
+
+    public Retangulo (double ladoA, double ladoB ) {
+        setLadoA(ladoA);
+        setLadoB(ladoB);
+
+    }
+
+    //GET e SET
+
+
+    public void setLadoA(double ladoA) {
+        if (validaA(ladoA)){
+            this.ladoA = ladoA;
+        } else {
+            //System.out.println("CPF INVALIDO");
+            System.out.println("Lado A invalido (fora do intervalo 0,00 a 20,00). Usando valor padrão 1. ");
+        }
+    }
+
+    public double getLadoA() {
+        return ladoA;
+    }
+
+    //Verfica A
+    private boolean validaA(double ladoA) {
+        return ladoA > 0 && ladoA <= 20;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public void setLadoB(double ladoB) {
+        if (validaA(ladoB)){
+            this.ladoB = ladoB;
+        } else {
+            //System.out.println("CPF INVALIDO");
+            System.out.println("Lado B invalido (fora do intervalo 0,00 a 20,00). Usando valor padrão 1.");
+        }
+    }
+
+    //exibir dados retangulo
+
+    public String exibirRetangulo() {return "Lado A:" + String.format("%.2f",ladoA) + " Lado B:" + String.format("%.2f",ladoB);}
+
+// calcula perímetro retângulo p=2*(a+b)
+
+    public String perimetro(){
+        double CalcPerimetro = 2 * (ladoA + ladoB);
+    return String.format("%.2f", CalcPerimetro);}
+
+    public String area(){
+        double CalcArea = (ladoA * ladoB);
+        return String.format("%.2f", CalcArea);}
+}
---
+++ M2exercicios/src/main/java/M2S02/ex3/TesteRetangulo.java
@@ -0,0 +1,18 @@
+package M2S02.ex3;
+
+public class TesteRetangulo {
+    public  static void main(String[] args){
+        try {
+
+        Retangulo novoRetangulo = new Retangulo(2,4);
+
+        System.out.println("As dimensões do Retângulo são: "+ novoRetangulo.exibirRetangulo());
+        System.out.println("O perímetro é: "+ novoRetangulo.perimetro());
+            System.out.println("A area é: "+ novoRetangulo.area());
+
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
