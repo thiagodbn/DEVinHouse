5aada3ab44ab7832be00e3f802dbdd5fbc104549
Noronha IntelliJ IDEA <thiagodbn@gmail.com>
2023-11-04 16:30:27 -0300

    M2S04 EX 2 Sobrescrita

---
+++ M2exercicios/src/main/java/M2S04/ex2/Funcionario.java
@@ -0,0 +1,7 @@
+package M2S04.ex2;
+
+public class Funcionario {
+    public String obterCargo(){
+        return "Sou Funcionário";
+    };
+}
---
+++ M2exercicios/src/main/java/M2S04/ex2/Gerente.java
@@ -0,0 +1,8 @@
+package M2S04.ex2;
+
+public final class Gerente extends Funcionario {
+    @Override
+    public String obterCargo() {
+        return "Sou gerente";
+    }
+}
